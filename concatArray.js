45d50e62b6141e46b61c861fe047b26e25490d52
natnichapin <Natnicha.pin@mail.kmutt.ac.th>
2022-09-09 22:53:39 +0700

    edit3

--- concatArray.js
+++ concatArray.js
@@ -2,11 +2,12 @@ const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
   array1=((array1!==[])?array1:undefined)??undefined
-  array2=((array2!==[])?array2:undefined)??undefined 
-  if(array1!==undefined||array2!==undefined){
-    return array1.concat(array2)
-  }
-  return undefined
+    array2=((array2!==[])?array2:undefined)??undefined
+    if(array1!==undefined&&array2!==undefined){
+      return array1.concat(array2)
+    }
+    let ans = array1??array2
+    return ans
   
 
 }
